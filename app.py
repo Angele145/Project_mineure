7b91bb91ca94c694c5e3494e2eea4385e4216ea7
Angele145 <angele.nicolaucarvalhinho@etu.unilasalle.fr>
2025-10-21 09:30:53 +0200

    Update and rename app.txt to app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.header("mon application
+
+bombres = [1, 2, 4, 7]
+carré = [1**2, 2**2, 4**2, 7**2]
+
+d = ("nombres":nombres, "carré":carré)
+data = pd.DataFrame(d)
+
+st.dataFrame(data)
