24f1dd802a7f9d27ec1233b8bebe1783e85f82e8
Angele145 <angele.nicolaucarvalhinho@etu.unilasalle.fr>
2025-10-21 09:34:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.header("mon application")
 bombres = [1, 2, 4, 7]
 carré = [1**2, 2**2, 4**2, 7**2]
 
-d = ("nombres":nombres, "carré":carré)
+d = ("nombres": nombres, "carré": carré)
 data = pd.DataFrame(d)
 
 st.dataFrame(data)
